1e5adaab23e6c4a5482b3665ab685482bcf3d61c
Armand <armandbernard1995@gmail.com>
2023-09-01 20:56:00 +0100

    migrate to .NET 7

--- iTunesParser/Extensions.cs
+++ iTunesParser/Extensions.cs
@@ -1,37 +1,34 @@
-﻿using System;
+﻿namespace iTunesSmartParser;
 
-namespace iTunesSmartParser
+internal static class Extensions
 {
-    internal static class Extensions
+    /// <summary>
+    /// Get sub array from array using index + length
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="data"></param>
+    /// <param name="index"></param>
+    /// <param name="length"></param>
+    /// <returns></returns>
+    public static T[] SubArrayL<T>(this T[] data, int index, int length)
     {
-        /// <summary>
-        /// Get sub array from array using index + length
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="data"></param>
-        /// <param name="index"></param>
-        /// <param name="length"></param>
-        /// <returns></returns>
-        public static T[] SubArrayL<T>(this T[] data, int index, int length)
-        {
-            T[] result = new T[length];
-            Array.Copy(data, index, result, 0, length);
-            return result;
-        }
+        T[] result = new T[length];
+        Array.Copy(data, index, result, 0, length);
+        return result;
+    }
 
-        /// <summary>
-        /// Get sub array from array using index + length
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="data"></param>
-        /// <param name="index"></param>
-        /// <param name="length"></param>
-        /// <returns></returns>
-        public static T[] SubArrayR<T>(this T[] data, int start, int end)
-        {
-            T[] result = new T[end - start];
-            Array.Copy(data, start, result, 0, end - start);
-            return result;
-        }
+    /// <summary>
+    /// Get sub array from array using index + length
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="data"></param>
+    /// <param name="index"></param>
+    /// <param name="length"></param>
+    /// <returns></returns>
+    public static T[] SubArrayR<T>(this T[] data, int start, int end)
+    {
+        T[] result = new T[end - start];
+        Array.Copy(data, start, result, 0, end - start);
+        return result;
     }
 }
