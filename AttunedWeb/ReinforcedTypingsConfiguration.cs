426070f2e7cd33a1c82ebed528f95f0c2716fb5e
Armand <armandbernard1995@gmail.com>
2023-09-03 21:48:41 +0100

    Create UTCDateTime branded string and use it for DateTime

--- AttunedWeb/ReinforcedTypingsConfiguration.cs
+++ AttunedWeb/ReinforcedTypingsConfiguration.cs
@@ -9,6 +9,6 @@ public static class ReinforcedTypingsConfiguration
     public static void Configure(ConfigurationBuilder builder)
     {
         builder.Global(config => config.UseModules());
-        builder.Substitute(typeof(DateTime), new RtSimpleTypeName("string"));
+        builder.Substitute(typeof(DateTime), new RtSimpleTypeName("UTCDateTime"));
     }
 }
---
+++ AttunedWeb/front-end/src/dtos/UTCDateTime.ts
@@ -0,0 +1,3 @@
+type UTCDateTime = string & {
+    __brand: "UTCDateTime"
+}
