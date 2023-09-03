d964f02ec76d4c3f6259084e50981814dd800ba6
Armand <armandbernard1995@gmail.com>
2023-09-03 16:41:36 +0100

    Add local only test for Playlists

--- iTunesParserTests/Xml/XmlParserTests.cs
+++ iTunesParserTests/Xml/XmlParserTests.cs
@@ -16,4 +16,14 @@ public class XmlParserTests
 
         tracks.Should().HaveCountGreaterThan(1);
     }
+    
+    [Test]
+    [Category("LocalOnly")]
+    // This test can only be run locally as a library is private
+    public void ParsePlaylists_WorksWithARealLibrary()
+    {
+        var playlists = _xmlParser.ParsePlaylists("iTunesMusicLibrary.xml").Result;
+
+        playlists.Should().HaveCountGreaterThan(1);
+    }
 }
