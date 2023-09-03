b0b744be85ba895fd0548e641dbe2b1d74f70243
Armand <armandbernard1995@gmail.com>
2023-09-03 21:41:21 +0100

    Configure Reinforced.Typings to not generate modules

---
+++ AttunedWeb/ReinforcedTypingsConfiguration.cs
@@ -0,0 +1,14 @@
+using Reinforced.Typings.Ast.TypeNames;
+
+namespace AttunedWebApi;
+
+using Reinforced.Typings.Fluent;
+
+public static class ReinforcedTypingsConfiguration
+{
+    public static void Configure(ConfigurationBuilder builder)
+    {
+        builder.Global(config => config.UseModules());
+        builder.Substitute(typeof(DateTime), new RtSimpleTypeName("string"));
+    }
+}
