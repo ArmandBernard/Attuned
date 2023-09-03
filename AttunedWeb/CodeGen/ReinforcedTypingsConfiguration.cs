a763a0cb2dc23c7b2f0573747a4e651f6e8b757a
Armand <armandbernard1995@gmail.com>
2023-09-03 22:42:47 +0100

    make nullable types potentially undefined

--- AttunedWeb/CodeGen/ReinforcedTypingsConfiguration.cs
+++ AttunedWeb/CodeGen/ReinforcedTypingsConfiguration.cs
@@ -4,11 +4,17 @@ using ConfigurationBuilder = Reinforced.Typings.Fluent.ConfigurationBuilder;
 
 namespace AttunedWebApi.CodeGen;
 
+// This is used by Reinforced.Typings.settings.xml
+// ReSharper disable once UnusedType.Global
 public static class ReinforcedTypingsConfiguration
 {
     public static void Configure(ConfigurationBuilder builder)
     {
         builder.Global(config => config.UseModules());
         builder.Substitute(typeof(DateTime), new RtSimpleTypeName("UTCDateTime"));
+        // if a type is nullable, use "type | undefined"
+        builder.SubstituteGeneric(typeof(Nullable<>),
+            (type, resolver) =>
+                new RtSimpleTypeName($"{resolver.ResolveTypeName(type.GenericTypeArguments.Single())} | undefined"));
     }
 }
