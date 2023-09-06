edfd8261dbc508658d0c609fcc5b7fce62f8a2b4
Armand <armandbernard1995@gmail.com>
2023-09-06 22:02:16 +0100

    Add react query wrapper stuff

--- AttunedWeb/front-end/src/App.tsx
+++ AttunedWeb/front-end/src/App.tsx
@@ -1,13 +1,16 @@
 import { TracksView } from "./Tracks/TracksView.tsx";
 import { ThemeLayer } from "./Theming/ThemeLayer.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeLayer>
         <h1>Tracks</h1>
         <TracksView />
       </ThemeLayer>
-    </>
+    </QueryClientProvider>
   );
 }
