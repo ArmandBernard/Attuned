51cb30b84205d93c6cb862ece37fdb51eed9a123
Armand <armandbernard1995@gmail.com>
2023-09-06 23:38:55 +0100

    Configure CORS

--- AttunedWeb/Program.cs
+++ AttunedWeb/Program.cs
@@ -4,9 +4,24 @@ namespace AttunedWebApi;
 
 internal static class Program
 {
+    private static string _corsPolicy = "corsPolicy";
+
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(_corsPolicy, policy =>
+            {
+                //policy.AllowAnyOrigin();
+                policy
+                    .WithOrigins("http://localhost:5173", "https://localhost:5173", "http://127.0.0.1:5173",
+                        "https://127.0.0.1:5173")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
 
         // Add services to the container.
         builder.Services.AddControllers().AddJsonOptions(options =>
@@ -15,6 +30,7 @@ internal static class Program
             options.JsonSerializerOptions.PropertyNamingPolicy = null;
             options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
         });
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -29,6 +45,8 @@ internal static class Program
         }
 
         app.UseHttpsRedirection();
+
+        app.UseCors(_corsPolicy);
 
         app.UseAuthorization();
 
