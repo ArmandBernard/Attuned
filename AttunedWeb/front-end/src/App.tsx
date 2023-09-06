cbeaccd17b820437bf74a56832e6166e18b72712
Armand <armandbernard1995@gmail.com>
2023-09-06 21:58:05 +0100

    Create a theme layer to simplify App.tsx

--- AttunedWeb/front-end/src/App.tsx
+++ AttunedWeb/front-end/src/App.tsx
@@ -1,26 +1,13 @@
-import {useCallback, useEffect, useRef} from "react";
-import {useSystemPreferredTheme} from "./useSystemPreferredTheme.ts";
+import { TracksView } from "./Tracks/TracksView.tsx";
+import { ThemeLayer } from "./Theming/ThemeLayer.tsx";
 
 export default function App() {
-  const htmlRef = useRef(document.documentElement);
-  
-  const systemPreferredTheme = useSystemPreferredTheme();
-  
-  // when the themePreference changes, also update the app theme
-  useEffect(() => {
-    setTheme(
-        systemPreferredTheme === "dark"
-    );
-  }, [systemPreferredTheme]);
-  
-  // set the site theme using a class on the main html element
-  const setTheme = useCallback((dark: boolean) => {
-    if (dark) {
-      htmlRef.current.classList.add("dark");
-    } else {
-      htmlRef.current.classList.remove("dark");
-    }
-  }, []);
-  
-  return <></>;
+  return (
+    <>
+      <ThemeLayer>
+        <h1>Tracks</h1>
+        <TracksView />
+      </ThemeLayer>
+    </>
+  );
 }
---
+++ AttunedWeb/front-end/src/Theming/ThemeLayer.tsx
@@ -0,0 +1,42 @@
+import {
+  createContext,
+  FunctionComponent,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+} from "react";
+import { useSystemPreferredTheme } from "./useSystemPreferredTheme.ts";
+
+const ThemeContext = createContext<"light" | "dark">("light");
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeLayer: FunctionComponent<{ children?: ReactNode }> = ({
+  children,
+}) => {
+  const htmlRef = useRef(document.documentElement);
+
+  const systemPreferredTheme = useSystemPreferredTheme();
+
+  // when the themePreference changes, also update the app theme
+  useEffect(() => {
+    setTheme(systemPreferredTheme === "dark");
+  }, [systemPreferredTheme]);
+
+  // set the site theme using a class on the main html element
+  const setTheme = useCallback((dark: boolean) => {
+    if (dark) {
+      htmlRef.current.classList.add("dark");
+    } else {
+      htmlRef.current.classList.remove("dark");
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={systemPreferredTheme || "light"}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
