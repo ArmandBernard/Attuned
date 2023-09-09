18e4d616965456c7e8811109e0dc70e37c2970de
Armand <armandbernard1995@gmail.com>
2023-09-09 15:46:57 +0100

    add main landmark

--- AttunedWeb/front-end/src/App.tsx
+++ AttunedWeb/front-end/src/App.tsx
@@ -8,7 +8,9 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeLayer>
-        <TracksView />
+        <main>
+          <TracksView />
+        </main>
       </ThemeLayer>
     </QueryClientProvider>
   );
