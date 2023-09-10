bffbefb4267b5f297db6e674181690d80b55a3c8
Armand <armandbernard1995@gmail.com>
2023-09-10 13:43:50 +0100

    Whitelist certain IRules for polymorphic serialization

--- AttunedWeb/Dtos/Rules/IRuleDto.cs
+++ AttunedWeb/Dtos/Rules/IRuleDto.cs
@@ -1,7 +1,15 @@
+using System.Text.Json.Serialization;
 using AttunedWebApi.Dtos.Logic;
 
 namespace AttunedWebApi.Dtos.Rules;
 
+[JsonDerivedType(typeof(BooleanRuleDto))]
+[JsonDerivedType(typeof(DateRuleDto))]
+[JsonDerivedType(typeof(DictionaryRuleDto))]
+[JsonDerivedType(typeof(IntRuleDto))]
+[JsonDerivedType(typeof(PlaylistRuleDto))]
+[JsonDerivedType(typeof(StringRuleDto))]
+[JsonDerivedType(typeof(TimeSpanRuleDto))]
 public interface IRuleDto
 {
     public OperatorDto RuleType { get; }
