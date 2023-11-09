758be4590bf3974f53ace89f5ebf8ca102d21f77
Armand <armandbernard1995@gmail.com>
2023-11-09 20:17:39 +0000

    Adjust dialog width

--- AttunedWeb/front-end/src/Tracks/TrackDetails.tsx
+++ AttunedWeb/front-end/src/Tracks/TrackDetails.tsx
@@ -18,7 +18,7 @@ export const TrackDetails: FunctionComponent<TrackDetailProps> = ({
 }) => {
   return (
     <Dialog
-      className="max-sm:w-full sm:max-w-[1000px] sm:w-4/5 bg-background text-text-color"
+      className="max-sm:w-full sm:max-w-[700px] sm:w-4/5 bg-background text-text-color"
       show={show}
       onClose={onClose}
     >
