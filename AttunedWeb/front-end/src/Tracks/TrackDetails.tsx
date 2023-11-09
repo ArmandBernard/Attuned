82e58ef5384b96be55f6048e8cb3c93f487500af
Armand <armandbernard1995@gmail.com>
2023-11-09 20:13:15 +0000

    Add a whole bunch of fields

--- AttunedWeb/front-end/src/Tracks/TrackDetails.tsx
+++ AttunedWeb/front-end/src/Tracks/TrackDetails.tsx
@@ -1,6 +1,9 @@
 import { Dialog } from "../Dialog.tsx";
 import { FunctionComponent, ReactNode } from "react";
 import { TrackDto } from "../dtos/Dtos.ts";
+import { getRatingString } from "../StringFormatters/getRatingString.ts";
+import { Rating } from "../dtos/Rating.ts";
+import { getLoveString } from "../StringFormatters/getLoveString.ts";
 
 interface TrackDetailProps {
   show: boolean;
@@ -20,12 +23,42 @@ export const TrackDetails: FunctionComponent<TrackDetailProps> = ({
       onClose={onClose}
     >
       <div className="flex justify-between py-3 px-4 text-xl">
-        <h1 className="">{track === undefined ? "No track selected" : track.Name }</h1>
+        <h1>{track === undefined ? "No track selected" : track.Name}</h1>
         <button onClick={onClose}>✕</button>
-      </div>      
+      </div>
       {track !== undefined && (
         <div className="grid grid-cols-[auto_1fr] p-4 gap-x-2">
           <FieldValuePair fieldName="Name">{track.Name}</FieldValuePair>
+          <FieldValuePair fieldName="Artist">{track.Artist}</FieldValuePair>
+          <FieldValuePair fieldName="Album">{track.Album}</FieldValuePair>
+          <FieldValuePair fieldName="Composer">{track.Composer}</FieldValuePair>
+          <FieldValuePair fieldName="Genre">{track.Genre}</FieldValuePair>
+          <FieldValuePair fieldName="Year">{track.Year}</FieldValuePair>
+          <FieldValuePair fieldName="Track">
+            {track.TrackNumber ?? "?"} of {track.TrackCount ?? "?"}
+          </FieldValuePair>
+          <FieldValuePair fieldName="Disc">
+            {track.DiscNumber ?? "?"} of {track.DiscCount ?? "?"}
+          </FieldValuePair>
+          <FieldValuePair fieldName="Rating">
+            <span className="flex gap-4">
+              <span
+                aria-label={`${track.Rating} stars`}
+                className="text-primary"
+              >
+                {getRatingString(track.Rating as Rating)}
+              </span>
+              <span
+                className="text-love"
+                aria-label={track.Loved ? "loved" : "not loved"}
+              >
+                {getLoveString(track.Loved)}
+              </span>
+            </span>
+          </FieldValuePair>
+          <FieldValuePair fieldName="Play count">
+            {track.PlayCount}
+          </FieldValuePair>
         </div>
       )}
     </Dialog>
@@ -34,13 +67,13 @@ export const TrackDetails: FunctionComponent<TrackDetailProps> = ({
 
 const FieldValuePair = ({
   fieldName,
-  children
+  children,
 }: {
   fieldName: string;
   children?: ReactNode;
 }) => (
   <>
     <span className="font-bold">{fieldName}</span>
-    {children}
+    <span>{children}</span>
   </>
 );
