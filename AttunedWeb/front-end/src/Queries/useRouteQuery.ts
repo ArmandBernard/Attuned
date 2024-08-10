2aa24bd990f51e4305692247dfbde00438fccc93
Armand <armandbernard1995@gmail.com>
2024-08-10 18:36:40 +0100

    fix route parameter handling

--- AttunedWeb/front-end/src/Queries/useRouteQuery.ts
+++ AttunedWeb/front-end/src/Queries/useRouteQuery.ts
@@ -42,12 +42,38 @@ const base = "http://localhost:5280";
 const buildUrl = (path: string, parameters: ValidParams | undefined) => {
   const url = new URL(path, base);
 
-  if (parameters !== undefined) {
-    Object.entries(parameters).forEach(([name, value]) => {
-      if (value !== undefined) {
-        url.searchParams.set(name, value.toString());
-      }
-    });
+  if (parameters === undefined) {
+    return url;
   }
-  return url;
+
+  let filledUrlString = url.toString();
+
+  // %7B is "{", %7D is "}"
+  const found = filledUrlString.matchAll(/%7B(\w+)%7D/g);
+
+  const handled: string[] = [];
+
+  for (const regExpExecArray of found) {
+    const name = regExpExecArray[1];
+
+    const value = parameters[name]?.toString();
+
+    if (value === undefined) {
+      throw new Error(`parameter ${name} not found in fetch ${url}`);
+    }
+
+    filledUrlString = filledUrlString.replace(`%7B${name}%7D`, value);
+
+    handled.push(name);
+  }
+
+  const filledUrl = new URL(filledUrlString);
+
+  Object.entries(parameters).forEach(([name, value]) => {
+    if (value !== undefined && !handled.includes(name)) {
+      filledUrl.searchParams.set(name, value.toString());
+    }
+  });
+
+  return filledUrl;
 };
