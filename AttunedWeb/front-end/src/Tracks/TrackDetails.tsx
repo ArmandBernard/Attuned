d8fa895423348e74bc522df10dda9d51037493a8
Armand <armandbernard1995@gmail.com>
2024-08-11 20:18:49 +0100

    Show album art

--- AttunedWeb/front-end/src/Tracks/TrackDetails.tsx
+++ AttunedWeb/front-end/src/Tracks/TrackDetails.tsx
@@ -37,107 +37,126 @@ export const TrackDetails: FunctionComponent<TrackDetailProps> = ({
   const availableDetails: TrackDetailsDto | TrackDto | undefined =
     trackDetails ?? track;
 
+  if (availableDetails === undefined) {
+    return <></>;
+  }
+
   return (
     <Dialog
       className="max-sm:w-full sm:max-w-[700px] sm:w-4/5 bg-background text-text-color"
       show={show}
       onClose={onClose}
     >
-      <div className="flex justify-between py-3 px-4 text-xl">
-        <h1>
-          {availableDetails === undefined
-            ? "No track selected"
-            : availableDetails.Name}
-        </h1>
-        <div className="flex gap-4">
-          {onPreviousTrack && (
-            <button
-              aria-label="Previous track"
-              title="Previous track"
-              onClick={onPreviousTrack}
-            >
-              ⮜
-            </button>
-          )}
-          {onNextTrack && (
-            <button
-              aria-label="Next track"
-              title="Next track"
-              onClick={onNextTrack}
-            >
-              ⮞
-            </button>
-          )}
-          <button aria-label="Close" title="Close" onClick={onClose}>
-            ✕
-          </button>
-        </div>
-      </div>
-      {availableDetails !== undefined && (
-        <div className="grid grid-cols-[auto_1fr] p-4 gap-x-2">
-          <FieldValuePair fieldName="Name">
-            {availableDetails.Name}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Artist">
-            {availableDetails.Artist}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Album">
-            {availableDetails.Album}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Composer">
-            {availableDetails.Composer}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Genre">
-            {availableDetails.Genre}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Year">
-            {availableDetails.Year}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Track">
-            {availableDetails.TrackNumber ?? "?"} of{" "}
-            {availableDetails.TrackCount ?? "?"}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Disc">
-            {availableDetails.DiscNumber ?? "?"} of{" "}
-            {availableDetails.DiscCount ?? "?"}
-          </FieldValuePair>
-          <FieldValuePair fieldName="Rating">
-            <span className="flex gap-4">
-              <span
-                aria-label={`${availableDetails.Rating} stars`}
-                className="text-primary"
-              >
-                {getRatingString(availableDetails.Rating as Rating)}
-              </span>
-              <span
-                className="text-love"
-                aria-label={getLoveLabel(availableDetails.Loved)}
-              >
-                <LoveStatus
-                  loved={availableDetails.Loved}
-                  className="w-4 h-4 inline-block"
-                />
-              </span>
-            </span>
-          </FieldValuePair>
-          <FieldValuePair fieldName="Play count">
-            {availableDetails.PlayCount}
-          </FieldValuePair>
-        </div>
-      )}
+      <Header
+        track={availableDetails}
+        onPreviousTrack={onPreviousTrack}
+        onNextTrack={onNextTrack}
+        onClose={onClose}
+      />
+      <Details track={availableDetails} />
     </Dialog>
   );
 };
 
-const FieldValuePair = ({
+function Header({
+  track,
+  onPreviousTrack,
+  onNextTrack,
+  onClose,
+}: {
+  track: TrackDto | TrackDetailsDto;
+  onPreviousTrack: (() => void) | undefined;
+  onNextTrack: (() => void) | undefined;
+  onClose: () => void;
+}) {
+  return (
+    <div className="flex justify-between py-3 px-4 text-xl bg-paper-1">
+      <div className="flex items-center gap-4">
+        {"CoverArt" in track ? (
+          <div className="w-32 h-32 flex items-center justify-center">
+            <img
+              alt="album art"
+              className="w-32 rounded"
+              src={`data:image/png;base64,${track.CoverArt}`}
+            />
+          </div>
+        ) : (
+          <div className="w-32 h-32 rounded bg-paper-2"></div>
+        )}
+        <div>
+          <h1 className="text-xl">{track.Name}</h1>
+          <div className="text-base">{track.Artist}</div>
+          <div className="text-base">{track.Album}</div>
+        </div>
+      </div>
+      <div className="flex items-start gap-4">
+        {onPreviousTrack && (
+          <button
+            aria-label="Previous track"
+            title="Previous track"
+            onClick={onPreviousTrack}
+          >
+            ⮜
+          </button>
+        )}
+        {onNextTrack && (
+          <button
+            aria-label="Next track"
+            title="Next track"
+            onClick={onNextTrack}
+          >
+            ⮞
+          </button>
+        )}
+        <button aria-label="Close" title="Close" onClick={onClose}>
+          ✕
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Details({ track }: { track: TrackDto }) {
+  return (
+    <div className="grid grid-cols-[auto_1fr] p-4 gap-x-2">
+      <FieldValuePair fieldName="Name">{track.Name}</FieldValuePair>
+      <FieldValuePair fieldName="Artist">{track.Artist}</FieldValuePair>
+      <FieldValuePair fieldName="Album">{track.Album}</FieldValuePair>
+      <FieldValuePair fieldName="Composer">{track.Composer}</FieldValuePair>
+      <FieldValuePair fieldName="Genre">{track.Genre}</FieldValuePair>
+      <FieldValuePair fieldName="Year">{track.Year}</FieldValuePair>
+      <FieldValuePair fieldName="Track">
+        {track.TrackNumber ?? "?"} of {track.TrackCount ?? "?"}
+      </FieldValuePair>
+      <FieldValuePair fieldName="Disc">
+        {track.DiscNumber ?? "?"} of {track.DiscCount ?? "?"}
+      </FieldValuePair>
+      <FieldValuePair fieldName="Rating">
+        <span className="flex gap-4">
+          <span aria-label={`${track.Rating} stars`} className="text-primary">
+            {getRatingString(track.Rating as Rating)}
+          </span>
+          <span className="text-love" aria-label={getLoveLabel(track.Loved)}>
+            <LoveStatus loved={track.Loved} className="w-4 h-4 inline-block" />
+          </span>
+        </span>
+      </FieldValuePair>
+      <FieldValuePair fieldName="Play count">{track.PlayCount}</FieldValuePair>
+    </div>
+  );
+}
+
+function FieldValuePair({
   fieldName,
   children,
 }: {
   fieldName: string;
   children?: ReactNode;
-}) => (
-  <>
-    <span className="font-bold">{fieldName}</span>
-    <span>{children}</span>
-  </>
-);
+}) {
+  return (
+    <>
+      <span className="font-bold">{fieldName}</span>
+      <span>{children}</span>
+    </>
+  );
+}
