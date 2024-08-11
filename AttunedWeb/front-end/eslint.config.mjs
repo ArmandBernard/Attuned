10d84d4504238a2b84be66ea257b8fe2baefe99e
Armand <armandbernard1995@gmail.com>
2024-08-11 11:34:09 +0100

    use non-compat plugin versions

--- AttunedWeb/front-end/eslint.config.mjs
+++ AttunedWeb/front-end/eslint.config.mjs
@@ -1,59 +1,61 @@
 import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import tsEslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
 });
 
 export default [
-  ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-)), {
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...fixupConfigRules(compat.extends("plugin:react-hooks/recommended")),
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
     plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
-        react: fixupPluginRules(react),
-        "react-hooks": fixupPluginRules(reactHooks),
+      react,
+      "react-hooks": fixupPluginRules(reactHooks),
     },
-
     languageOptions: {
-        globals: {
-            ...globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
-
     rules: {
-        indent: ["warn", 2, {
-            SwitchCase: 1,
-        }],
+      indent: [
+        "warn",
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
 
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-        eqeqeq: "error",
-        "react-hooks/exhaustive-deps": ["error"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      eqeqeq: "error",
+      "react-hooks/exhaustive-deps": ["error"],
     },
-}, {
+  },
+  {
     files: ["src/dtos/Dtos.ts"],
 
     rules: {
-        quotes: "off",
+      quotes: "off",
     },
-}];
+  },
+];
